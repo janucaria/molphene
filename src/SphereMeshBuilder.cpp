20fae0fad32d0accb31ad9141f0b41a50563eacf
Januar C. Andaria <janucaria@gmail.com>
2014-05-26 17:57:57 +0800

    Modified methods for build sphere mesh.

--- src/SphereMeshBuilder.cpp
+++ src/SphereMeshBuilder.cpp
@@ -5,7 +5,7 @@ namespace molphene {
     SphereMeshBuilder::SphereMeshBuilder(unsigned int totalModels) :
         positions(nullptr),
         colors(nullptr),
-        latDiv(10),
+        latDiv(20),
         lonDiv(10)
     {
         unsigned int verticesPerModel = getVerticesSize();
@@ -35,7 +35,7 @@ namespace molphene {
     }
     
     unsigned int SphereMeshBuilder::getVerticesSize() {
-        return (latDiv + 1) * (lonDiv + 1);
+        return lonDiv * (latDiv + 1) * 2 + lonDiv * 2;
     }
     
     unsigned int SphereMeshBuilder::getTotalVertices() {
@@ -58,25 +58,57 @@ namespace molphene {
         
         idx *= getVerticesSize();
         
-        for(unsigned int i = 0; i <= lonDiv; ++i) {
+        for(unsigned int i = 0; i < lonDiv; ++i) {
             float theta = 1.0f * i * M_PI / lonDiv;
             float sinTheta = sin(theta);
             float cosTheta = cos(theta);
+            
+            float nextTheta = 1.0f * (i + 1) * M_PI / lonDiv;
+            float nextSinTheta = sin(nextTheta);
+            float nextCosTheta = cos(nextTheta);
             
             for (unsigned int j = 0; j <= latDiv; ++j) {
                 float phi = 2.0f * M_PI * j / latDiv;
                 float sinPhi = sin(phi);
                 float cosPhi = cos(phi);
                 
-                float x = cosPhi * sinTheta;
-                float y = sinPhi * sinTheta;
-                float z = cosTheta;
+                float x, y, z;
+                
+                x = cosPhi * sinTheta;
+                y = sinPhi * sinTheta;
+                z = cosTheta;
                 
                 positions[idx] = pos;
                 normals[idx] = vec3f(x, y, z) * rad;
                 colors[idx] = col;
                 
                 idx++;
+                
+                if(j == 0) {
+                    positions[idx] = pos;
+                    normals[idx] = vec3f(x, y, z) * rad;
+                    colors[idx] = col;
+                    
+                    idx++;
+                }
+                
+                x = cosPhi * nextSinTheta;
+                y = sinPhi * nextSinTheta;
+                z = nextCosTheta;
+                
+                positions[idx] = pos;
+                normals[idx] = vec3f(x, y, z) * rad;
+                colors[idx] = col;
+                
+                idx++;
+                
+                if(j == latDiv) {
+                    positions[idx] = pos;
+                    normals[idx] = vec3f(x, y, z) * rad;
+                    colors[idx] = col;
+                    
+                    idx++;
+                }
             }
         }
     }
