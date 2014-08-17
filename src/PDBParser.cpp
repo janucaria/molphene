31f1e1f51fa50f98f428cc3910af78122a49afdc
Januar C. Andaria <janucaria@gmail.com>
2014-08-17 15:48:38 +0800

    Parse field MODEL of PDB file format.

--- src/PDBParser.cpp
+++ src/PDBParser.cpp
@@ -6,8 +6,7 @@ namespace molphene {
     }
     
     void PDBParser::parse(Molecule & mol, std::istream & stream) {
-        
-        Model * currentModelPtr = &mol.addModel();
+        Model * currentModelPtr = nullptr;
         Chain * currentChainPtr = nullptr;
         Compound * currentCompoundPtr = nullptr;
         
@@ -17,24 +16,29 @@ namespace molphene {
             
             recordName = boost::trim_right_copy(column(1, 6));
             
-            //    COLUMNS        DATA  TYPE    FIELD        DEFINITION
-            //    -------------------------------------------------------------------------------------
-            //    1 -  6        Record name   "ATOM  "
-            //    7 - 11        Integer       serial       Atom  serial number.
-            //    13 - 16        Atom          name         Atom name.
-            //    17             Character     altLoc       Alternate location indicator.
-            //    18 - 20        Residue name  resName      Residue name.
-            //    22             Character     chainID      Chain identifier.
-            //    23 - 26        Integer       resSeq       Residue sequence number.
-            //    27             AChar         iCode        Code for insertion of residues.
-            //    31 - 38        Real(8.3)     x            Orthogonal coordinates for X in Angstroms.
-            //    39 - 46        Real(8.3)     y            Orthogonal coordinates for Y in Angstroms.
-            //    47 - 54        Real(8.3)     z            Orthogonal coordinates for Z in Angstroms.
-            //    55 - 60        Real(6.2)     occupancy    Occupancy.
-            //    61 - 66        Real(6.2)     tempFactor   Temperature  factor.
-            //    77 - 78        LString(2)    element      Element symbol, right-justified.
-            //    79 - 80        LString(2)    charge       Charge  on the atom.
-            if(recordName.compare("ATOM") == 0 || recordName.compare("HETATM") == 0) {
+            if(recordName.compare("MODEL") == 0) {
+                currentModelPtr = &mol.addModel();
+                currentChainPtr = nullptr;
+                
+            } else if(recordName.compare("ATOM") == 0 || recordName.compare("HETATM") == 0) {
+                
+                //    COLUMNS        DATA  TYPE    FIELD        DEFINITION
+                //    -------------------------------------------------------------------------------------
+                //    1 -  6        Record name   "ATOM  "
+                //    7 - 11        Integer       serial       Atom  serial number.
+                //    13 - 16        Atom          name         Atom name.
+                //    17             Character     altLoc       Alternate location indicator.
+                //    18 - 20        Residue name  resName      Residue name.
+                //    22             Character     chainID      Chain identifier.
+                //    23 - 26        Integer       resSeq       Residue sequence number.
+                //    27             AChar         iCode        Code for insertion of residues.
+                //    31 - 38        Real(8.3)     x            Orthogonal coordinates for X in Angstroms.
+                //    39 - 46        Real(8.3)     y            Orthogonal coordinates for Y in Angstroms.
+                //    47 - 54        Real(8.3)     z            Orthogonal coordinates for Z in Angstroms.
+                //    55 - 60        Real(6.2)     occupancy    Occupancy.
+                //    61 - 66        Real(6.2)     tempFactor   Temperature  factor.
+                //    77 - 78        LString(2)    element      Element symbol, right-justified.
+                //    79 - 80        LString(2)    charge       Charge  on the atom.
                 
                 unsigned int aserial = getInteger(7, 11);
                 std::string aname = boost::trim_copy(column(13, 16));
@@ -47,6 +51,11 @@ namespace molphene {
                 float ay = getReal(39, 46);
                 float az = getReal(47, 54);
                 std::string aelement = boost::trim_left_copy(column(77, 78));
+                
+                if(!currentModelPtr) {
+                    currentModelPtr = &mol.addModel();
+                    currentChainPtr = nullptr;
+                }
                 
                 if(!currentChainPtr || currentChainPtr->getId() != achainID) {
                     try {
@@ -71,8 +80,8 @@ namespace molphene {
                 Atom & atom = currentCompoundPtr->addAtom(aelement, aname, aserial);
                 atom.setPosition(ax, ay, az);
                 atom.setAltLoc(aaltLoc);
+                
             }
-            
         }
     }
     
@@ -85,7 +94,11 @@ namespace molphene {
     }
     
     unsigned int PDBParser::getInteger(unsigned int start,  unsigned int end) {
-        return static_cast<unsigned int>(std::stoul(column(start, end)));
+        try {
+            return static_cast<unsigned int>(std::stoul(column(start, end)));
+        } catch (const std::invalid_argument & ia) {
+            return 0;
+        }
     }
     
     char PDBParser::getChar(unsigned int start) {
