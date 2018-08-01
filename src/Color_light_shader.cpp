cbee52eec2a414320d60050bbc9c6eebb048e7fa
Januar Andaria <janucaria@gmail.com>
2018-08-01 09:50:28 +0800

    Lighting shader ready for multiple light sources

--- src/Color_light_shader.cpp
+++ src/Color_light_shader.cpp
@@ -175,55 +175,11 @@ Color_light_shader::frag_shader_source() const noexcept
     }
 
     void main() {
+      vec4 texRgba = texture2D(u_TexColor, v_ColorTexCoord.st);
       bool isDirLight = u_LightSource_radius < 0.;
 
       vec3 N = normalize(v_Normal);
       vec3 V = normalize(v_Position);
-      
-      float attenuationi = 0.;
-      float spoti = 1.;
-      vec3 L;
-      if(isDirLight) {
-        L = -u_LightSource_direction;
-        attenuationi = 1.;
-      } else {
-        vec3 distLP = u_LightSource_position - v_Position;
-        float dL = length(distLP);
-        L = normalize(distLP);
-
-        if(length(u_LightSource_attenuation) != 0. && dL <= u_LightSource_radius) {
-          float c1 = u_LightSource_attenuation.x;
-          float c2 = u_LightSource_attenuation.y;
-          float c3 = u_LightSource_attenuation.z;
-
-          attenuationi = 1. / max(c1 + c2 * dL + c3 * (dL * dL), 1.);
-
-          if(length(u_LightSource_direction) != 0.) {
-            vec3 spotDiri = normalize(u_LightSource_direction);
-            float spotAngle = acos(max(dot(-L, spotDiri), 0.));
-
-            float spotBW = u_LightSource_beamWidth;
-            float spotCO = u_LightSource_cutOffAngle;
-
-            if(spotAngle >= spotCO) {
-              spoti = 0.;
-            } else if(spotAngle <= spotBW) {
-              spoti = 1.;
-            } else if((spotBW < spotAngle) && (spotAngle < spotCO)) {
-              spoti = (spotAngle - spotCO ) / (spotBW - spotCO);
-            }
-          }
-        }
-      }
-
-      float dV = length(v_Position);
-      float f0 = fogInterpolant(dV);
-      vec3 IFrgb = u_Fog_color.rgb;
-
-      vec4 texRgba = texture2D(u_TexColor, v_ColorTexCoord.st);
-      vec3 ILrgb = u_LightSource_color.rgb;
-      float Ii = u_LightSource_intensity;
-      float Iia = u_LightSource_ambientIntensity;
 
       float Oa = u_Material_ambientIntensity;
       vec3 ODrgb = texRgba.rgb * u_Material_diffuseColor.rgb;
@@ -231,12 +187,60 @@ Color_light_shader::frag_shader_source() const noexcept
       vec3 OSrgb = u_Material_specularColor.rgb;
       float shininess = u_Material_shininess;
 
-      vec3 ambienti = Iia * ODrgb * Oa;
-      vec3 diffusei = Ii * ODrgb * dot( N, L );
-      vec3 speculari = Ii * OSrgb * pow(dot( N , ((L + V) / normalize(L + V))), shininess * 128.);
+      float dV = length(v_Position);
+      float f0 = fogInterpolant(dV);
+      vec3 IFrgb = u_Fog_color.rgb;
       
-      vec3 Irgb = IFrgb * (1. -f0)
-              + f0 * (OErgb + (attenuationi * spoti * ILrgb * (ambienti + diffusei + speculari)));
+      vec3 sumLights = vec3(0.);
+      for(int i = 0; i < 1; ++i) {
+        float attenuationi = 0.;
+        float spoti = 1.;
+        vec3 L;
+        if(isDirLight) {
+          L = -u_LightSource_direction;
+          attenuationi = 1.;
+        } else {
+          vec3 distLP = u_LightSource_position - v_Position;
+          float dL = length(distLP);
+          L = normalize(distLP);
+
+          if(length(u_LightSource_attenuation) != 0. && dL <= u_LightSource_radius) {
+            float c1 = u_LightSource_attenuation.x;
+            float c2 = u_LightSource_attenuation.y;
+            float c3 = u_LightSource_attenuation.z;
+
+            attenuationi = 1. / max(c1 + c2 * dL + c3 * (dL * dL), 1.);
+
+            if(length(u_LightSource_direction) != 0.) {
+              vec3 spotDiri = normalize(u_LightSource_direction);
+              float spotAngle = acos(max(dot(-L, spotDiri), 0.));
+
+              float spotBW = u_LightSource_beamWidth;
+              float spotCO = u_LightSource_cutOffAngle;
+
+              if(spotAngle >= spotCO) {
+                spoti = 0.;
+              } else if(spotAngle <= spotBW) {
+                spoti = 1.;
+              } else if((spotBW < spotAngle) && (spotAngle < spotCO)) {
+                spoti = (spotAngle - spotCO ) / (spotBW - spotCO);
+              }
+            }
+          }
+        }
+
+        vec3 ILrgb = u_LightSource_color.rgb;
+        float Ii = u_LightSource_intensity;
+        float Iia = u_LightSource_ambientIntensity;
+
+        vec3 ambienti = Iia * ODrgb * Oa;
+        vec3 diffusei = Ii * ODrgb * dot( N, L );
+        vec3 speculari = Ii * OSrgb * pow(dot( N , ((L + V) / normalize(L + V))), shininess * 128.);
+      
+        sumLights += (attenuationi * spoti * ILrgb * (ambienti + diffusei + speculari));
+      }
+
+      vec3 Irgb = IFrgb * (1. -f0) + f0 * (OErgb + sumLights);
       float A = texRgba.a;
 
       gl_FragColor = vec4(vec3(Irgb), A);
