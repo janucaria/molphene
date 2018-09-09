caa5804932133a71397362775da8377c4e23bd40
Januar Andaria <janucaria@gmail.com>
2018-09-09 18:26:57 +0800

    test: change C++ testing framework from Boost.Test to Catch2
    Change made because currently Boost.Test seems like not support for
    emscripten, but Catch2 did.

--- test/color.cpp
+++ test/color.cpp
@@ -1,32 +1,23 @@
 #include <cfloat>
 #include <type_traits>
 
-#define BOOST_TEST_MODULE "m3d color"
-#define BOOST_TEST_DYN_LINK
-#include <boost/test/unit_test.hpp>
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 
 #include <molphene/m3d.hpp>
 
-BOOST_AUTO_TEST_SUITE(rgba8_test)
-
-BOOST_AUTO_TEST_CASE(traits)
+TEST_CASE("rgba8_test traits")
 {
   using Rgba8 = molphene::Rgba8;
 
-  BOOST_CHECK(std::is_trivial_v<Rgba8>);
-  BOOST_CHECK(std::is_standard_layout_v<Rgba8>);
+  REQUIRE(std::is_trivial_v<Rgba8>);
+  REQUIRE(std::is_standard_layout_v<Rgba8>);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE(rgba32f_test)
-
-BOOST_AUTO_TEST_CASE(traits)
+TEST_CASE("rgba32f_test traits")
 {
   using Rgba32f = molphene::Rgba32f;
 
-  BOOST_CHECK(std::is_trivial_v<Rgba32f>);
-  BOOST_CHECK(std::is_standard_layout_v<Rgba32f>);
+  REQUIRE(std::is_trivial_v<Rgba32f>);
+  REQUIRE(std::is_standard_layout_v<Rgba32f>);
 }
-
-BOOST_AUTO_TEST_SUITE_END()
--- test/vector3.cpp
+++ test/vector3.cpp
@@ -1,125 +1,120 @@
 #include <cfloat>
 #include <type_traits>
 
-#define BOOST_TEST_MODULE "m3d vector"
-#define BOOST_TEST_DYN_LINK
-#include <boost/test/floating_point_comparison.hpp>
-#include <boost/test/unit_test.hpp>
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 
 #include <molphene/m3d.hpp>
 
-BOOST_AUTO_TEST_SUITE(float_vector3_test)
-
-BOOST_AUTO_TEST_CASE(traits)
+TEST_CASE("float_vector3_test traits")
 {
   using Vec3f = molphene::Vec3<float>;
 
-  BOOST_CHECK(std::is_trivial_v<Vec3f>);
-  BOOST_CHECK(std::is_standard_layout_v<Vec3f>);
+  REQUIRE(std::is_trivial_v<Vec3f>);
+  REQUIRE(std::is_standard_layout_v<Vec3f>);
 }
 
-BOOST_AUTO_TEST_CASE(initialization)
+TEST_CASE("float_vector3_test initialization")
 {
   using Vec3f = molphene::Vec3<float>;
 
   {
     auto vt = Vec3f{0};
-    BOOST_CHECK_EQUAL(vt.x, 0.0f);
-    BOOST_CHECK_EQUAL(vt.y, 0.0f);
-    BOOST_CHECK_EQUAL(vt.z, 0.0f);
+    REQUIRE(vt.x == 0.0f);
+    REQUIRE(vt.y == 0.0f);
+    REQUIRE(vt.z == 0.0f);
   }
   {
     auto vt = Vec3f{1.5, 2.2, 8.9};
-    BOOST_CHECK_EQUAL(vt.x, 1.5f);
-    BOOST_CHECK_EQUAL(vt.y, 2.2f);
-    BOOST_CHECK_EQUAL(vt.z, 8.9f);
+    REQUIRE(vt.x == 1.5f);
+    REQUIRE(vt.y == 2.2f);
+    REQUIRE(vt.z == 8.9f);
 
     vt = Vec3f{2.5, 0.3, 7.4};
-    BOOST_CHECK_EQUAL(vt.x, 2.5f);
-    BOOST_CHECK_EQUAL(vt.y, 0.3f);
-    BOOST_CHECK_EQUAL(vt.z, 7.4f);
+    REQUIRE(vt.x == 2.5f);
+    REQUIRE(vt.y == 0.3f);
+    REQUIRE(vt.z == 7.4f);
   }
   {
     auto vt = Vec3f{Vec3f{1.5, 2.3, 6.4}};
-    BOOST_CHECK_EQUAL(vt.x, 1.5f);
-    BOOST_CHECK_EQUAL(vt.y, 2.3f);
-    BOOST_CHECK_EQUAL(vt.z, 6.4f);
+    REQUIRE(vt.x == 1.5f);
+    REQUIRE(vt.y == 2.3f);
+    REQUIRE(vt.z == 6.4f);
   }
 }
 
-BOOST_AUTO_TEST_CASE(comparison)
+TEST_CASE("float_vector3_test comparison")
 {
   using Vec3f = molphene::Vec3<float>;
 
-  BOOST_CHECK((Vec3f{0} == Vec3f{0, 0, 0}));
-  BOOST_CHECK((Vec3f{3.5} == Vec3f{3.5, 3.5, 3.5}));
-  BOOST_CHECK((Vec3f{-.5} == Vec3f{-.5, -.5, -.5}));
+  REQUIRE((Vec3f{0} == Vec3f{0, 0, 0}));
+  REQUIRE((Vec3f{3.5} == Vec3f{3.5, 3.5, 3.5}));
+  REQUIRE((Vec3f{-.5} == Vec3f{-.5, -.5, -.5}));
 
-  BOOST_CHECK((Vec3f{-.5} != Vec3f{-.5, -.1, -.5}));
+  REQUIRE((Vec3f{-.5} != Vec3f{-.5, -.1, -.5}));
 }
 
-BOOST_AUTO_TEST_CASE(addition)
+TEST_CASE("float_vector3_test addition")
 {
   using Vec3f = molphene::Vec3<float>;
 
   {
     auto vt = Vec3f{0} + Vec3f{2.5, 0.3, 7.4};
-    BOOST_CHECK_CLOSE_FRACTION(vt.x, 2.5f, FLT_EPSILON);
-    BOOST_CHECK_CLOSE_FRACTION(vt.y, 0.3f, FLT_EPSILON);
-    BOOST_CHECK_CLOSE_FRACTION(vt.z, 7.4f, FLT_EPSILON);
+    REQUIRE(vt.x == Approx(2.5f));
+    REQUIRE(vt.y == Approx(0.3f));;
+    REQUIRE(vt.z == Approx(7.4f));;
   }
   {
     auto vt = Vec3f{-10.4, 0.4, -15.4};
     vt += Vec3f{0};
-    BOOST_CHECK_CLOSE_FRACTION(vt.x, -10.4f, FLT_EPSILON);
-    BOOST_CHECK_CLOSE_FRACTION(vt.y, 0.4f, FLT_EPSILON);
-    BOOST_CHECK_CLOSE_FRACTION(vt.z, -15.4f, FLT_EPSILON);
+    REQUIRE(vt.x == Approx(-10.4f));;
+    REQUIRE(vt.y == Approx(0.4f));;
+    REQUIRE(vt.z == Approx(-15.4f));;
   }
   {
     auto vt = Vec3f{-1., 0, 1} + 0.2;
-    BOOST_CHECK_CLOSE_FRACTION(vt.x, -0.8f, FLT_EPSILON);
-    BOOST_CHECK_CLOSE_FRACTION(vt.y, 0.2f, FLT_EPSILON);
-    BOOST_CHECK_CLOSE_FRACTION(vt.z, 1.2f, FLT_EPSILON);
+    REQUIRE(vt.x == Approx(-0.8f));;
+    REQUIRE(vt.y == Approx(0.2f));;
+    REQUIRE(vt.z == Approx(1.2f));;
   }
   {
     auto vt = Vec3f{10.4, -2.2, 5.5};
     vt += 3.2f;
-    BOOST_CHECK_CLOSE_FRACTION(vt.x, 13.6f, FLT_EPSILON);
-    BOOST_CHECK_CLOSE_FRACTION(vt.y, 1.0f, FLT_EPSILON);
-    BOOST_CHECK_CLOSE_FRACTION(vt.z, 8.7f, FLT_EPSILON);
+    REQUIRE(vt.x == Approx(13.6f));;
+    REQUIRE(vt.y == Approx(1.0f));;
+    REQUIRE(vt.z == Approx(8.7f));;
   }
 }
 
-BOOST_AUTO_TEST_CASE(vector3d_subtraction)
+TEST_CASE("float_vector3_test vector3d_subtraction")
 {
   using Vec3f = molphene::Vec3<float>;
 
   {
     auto vt = Vec3f{1.5, 2.2, 8.9} - Vec3f{0};
-    BOOST_CHECK_CLOSE_FRACTION(vt.x, 1.5f, FLT_EPSILON);
-    BOOST_CHECK_CLOSE_FRACTION(vt.y, 2.2f, FLT_EPSILON);
-    BOOST_CHECK_CLOSE_FRACTION(vt.z, 8.9f, FLT_EPSILON);
+    REQUIRE(vt.x == Approx(1.5f));;
+    REQUIRE(vt.y == Approx(2.2f));;
+    REQUIRE(vt.z == Approx(8.9f));;
   }
   {
     auto vt = Vec3f{0};
     vt -= Vec3f(-10.4, 0.4, -15.4);
-    BOOST_CHECK_CLOSE_FRACTION(vt.x, 10.4f, FLT_EPSILON);
-    BOOST_CHECK_CLOSE_FRACTION(vt.y, -0.4f, FLT_EPSILON);
-    BOOST_CHECK_CLOSE_FRACTION(vt.z, 15.4f, FLT_EPSILON);
+    REQUIRE(vt.x == Approx(10.4f));;
+    REQUIRE(vt.y == Approx(-0.4f));;
+    REQUIRE(vt.z == Approx(15.4f));;
   }
   {
     auto vt = Vec3f{2.5, -0.3, 7.4} - 0.2;
-    BOOST_CHECK_CLOSE_FRACTION(vt.x, 2.3f, FLT_EPSILON);
-    BOOST_CHECK_CLOSE_FRACTION(vt.y, -0.5f, FLT_EPSILON);
-    BOOST_CHECK_CLOSE_FRACTION(vt.z, 7.2f, FLT_EPSILON);
+    REQUIRE(vt.x == Approx(2.3f));;
+    REQUIRE(vt.y == Approx(-0.5f));;
+    REQUIRE(vt.z == Approx(7.2f));;
   }
   {
     auto vt = Vec3f{1.5, 2.2, 8.9};
     vt -= 3.2f;
-    BOOST_CHECK_CLOSE_FRACTION(vt.x, -1.7f, FLT_EPSILON);
-    BOOST_CHECK_CLOSE_FRACTION(vt.y, -1.f, FLT_EPSILON);
-    BOOST_CHECK_CLOSE_FRACTION(vt.z, 5.7f, FLT_EPSILON);
+    REQUIRE(vt.x == Approx(-1.7f));;
+    REQUIRE(vt.y == Approx(-1.f));;
+    REQUIRE(vt.z == Approx(5.7f));;
   }
 }
 
-BOOST_AUTO_TEST_SUITE_END()
