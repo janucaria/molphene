350a21c8e1d51ee62ee9ba89c12a63bb88813b37
Januar Andaria <janucaria@gmail.com>
2019-02-19 21:22:17 +0800

    refactor: create base class for application

--- bins/glfw/src/application.cpp
+++ bins/glfw/src/application.cpp
@@ -18,7 +18,8 @@ void application::init_context()
   int width = 640;
   int height = 480;
 
-  auto* win_ptr = glfwCreateWindow(width, height, "Simple example", nullptr, nullptr);
+  auto* win_ptr =
+   glfwCreateWindow(width, height, "Simple example", nullptr, nullptr);
 
   if(!win_ptr) {
     glfwTerminate();
@@ -26,7 +27,7 @@ void application::init_context()
   }
 
   window_.reset(win_ptr);
-   
+
   glfwMakeContextCurrent(window_.get());
 
   glfwSetWindowUserPointer(window_.get(), this);
@@ -39,7 +40,7 @@ void application::init_context()
      if(action == GLFW_PRESS) {
        auto charcode = key;
        if(charcode == GLFW_KEY_ESCAPE) {
-        charcode = 27;
+         charcode = 27;
        }
        app->key_press_event(charcode, mods);
      }
@@ -84,81 +85,6 @@ void application::close_app()
   glfwSetWindowShouldClose(window_.get(), GL_TRUE);
 }
 
-void application::key_press_event(unsigned char charcode, int mods)
-{
-  switch(charcode) {
-  case 27:
-    close_app();
-    break;
-  case 80: 
-  case 112:
-    camera.projection_mode(true);
-    break;
-  case 79:
-  case 111:
-    camera.projection_mode(false);
-    break;
-  case 75:
-  case 107:
-    scene.representation(molphene::molecule_display::spacefill, molecule);
-    break;
-  case 76:
-  case 108:
-    scene.representation(molphene::molecule_display::ball_and_stick, molecule);
-    break;
-  }
-}
-
-void application::mouse_press_event(int button, int mods, int pos_x, int pos_y)
-{
-  click_state.is_down = true;
-  click_state.last_x = pos_x;
-  click_state.last_y = pos_y;
-}
-
-void application::mouse_release_event(int button, int mods, int pos_x, int pos_y)
-{
-  click_state.is_down = false;
-  click_state.last_x = pos_x;
-  click_state.last_y = pos_y;
-}
-
-void application::mouse_move_event(int pos_x, int pos_y)
-{
-  if(click_state.is_down) {
-    const auto delta_x = static_cast<double>(click_state.last_x) - pos_x;
-    const auto delta_y = static_cast<double>(click_state.last_y) - pos_y;
-
-    click_state.last_x = pos_x;
-    click_state.last_y = pos_y;
-
-    scene.rotate({M_PI * delta_y / 180, M_PI * delta_x / 180, 0});
-  }
-}
-
-void application::mouse_scroll_event(int offset_x, int offset_y)
-{
-  offset_y > 0 ? camera.zoom_in() : camera.zoom_out();
-}
-
-void application::framebuffer_size_change_event(int width, int height)
-{
-  renderer.change_dimension(width, height);
-  camera.aspect_ratio(width, height);
-  camera.update_view_matrix();
-}
-
-void application::open_pdb_data(std::string pdbdata)
-{
-  std::stringstream pdbstm;
-  pdbstm.str(pdbdata);
-  molecule = molphene::chemdoodle_json_parser{}.parse(pdbstm);
-
-  scene.reset_mesh(molecule);
-  camera.top = scene.bounding_sphere().radius() + 2;
-  camera.update_view_matrix();
-}
-
 void application::canvas_size_change_callback(int width, int height)
 {
   renderer.change_dimension(width, height);
@@ -180,33 +106,18 @@ void application::change_representation(int representation_type)
 
 void application::render_frame()
 {
-  renderer.render(scene, camera);
+  base_application_type::render_frame();
 
   glfwSwapBuffers(window_.get());
   glfwPollEvents();
 }
 
-void application::setup()
+auto application::framebuffer_size() const -> framebuffer_size_type
 {
-  init_context();
-
-  scene.setup_graphics();
-  renderer.init();
-
-  const auto [width, height] = [](auto window) noexcept
-  {
-    int width;
-    int height;
-    glfwGetFramebufferSize(window, &width, &height);
-    return std::make_pair(width, height);
-  }
-  (window_.get());
-
-  renderer.change_dimension(width, height);
-  camera.aspect_ratio(width, height);
-  camera.update_view_matrix();
-
-  scene.reset_mesh(molecule);
+  int width;
+  int height;
+  glfwGetFramebufferSize(window_.get(), &width, &height);
+  return std::make_pair(width, height);
 }
 
 void application::run()
--- bins/glfw/src/application.hpp
+++ bins/glfw/src/application.hpp
@@ -4,18 +4,11 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
-#include <molecule/chemdoodle_json_parser.hpp>
-#include <molecule/molecule.hpp>
-
-#include <molphene/gl_renderer.hpp>
-#include <molphene/scene.hpp>
-#include <molphene/stdafx.hpp>
-
-#include <molphene/io/click_state.hpp>
+#include "basic_application.hpp"
 
 namespace molphene {
 
-class application {
+class application : public basic_application<application> {
   struct glfw_window_guard {
     void operator()(GLFWwindow* window) const noexcept
     {
@@ -26,14 +19,14 @@ class application {
 
   using glfw_window_pointer = std::unique_ptr<GLFWwindow, glfw_window_guard>;
 
+  using base_application_type = basic_application<application>;
+
 public:
+  using framebuffer_size_type = std::pair<std::size_t, std::size_t>;
+
   void init_context();
 
-  void setup();
-
   void run();
-
-  void open_pdb_data(std::string pdbdata);
 
   void canvas_size_change_callback(int width, int height);
 
@@ -41,32 +34,12 @@ public:
 
   void render_frame();
 
+  auto framebuffer_size() const -> framebuffer_size_type;
+
   void close_app();
 
-  void mouse_press_event(int button, int mods, int pos_x, int pos_y);
-
-  void mouse_release_event(int button, int mods, int pos_x, int pos_y);
-
-  void mouse_move_event(int pos_x, int pos_y);
-
-  void mouse_scroll_event(int pos_x, int pos_y);
-
-  void key_press_event(unsigned char charcode, int mods);
-
-  void framebuffer_size_change_event(int width, int height);
 private:
-
-  io::click_state click_state{false, 0, 0};
-
   glfw_window_pointer window_;
-
-  molphene::scene scene{};
-
-  molphene::gl_renderer renderer;
-
-  molphene::scene::camera camera;
-
-  molphene::molecule molecule;
 };
 
 } // namespace molphene
