7c0c46f7e81f3d0f09a7e062d546c58c8590406a
Januar Andaria <janucaria@gmail.com>
2019-02-23 11:00:12 +0800

    feat: add void template spesialization for type_configs

---
+++ libs/core/src/molphene/detail/empty.hpp
@@ -0,0 +1,11 @@
+#ifndef MOLPHENE_DETAIL_EMPTY_HPP
+#define MOLPHENE_DETAIL_EMPTY_HPP
+
+namespace molphene::detail {
+
+struct empty {
+};
+
+} // namespace molphene::detail
+
+#endif
--- libs/core/src/molphene/type_configs.hpp
+++ libs/core/src/molphene/type_configs.hpp
@@ -1,6 +1,10 @@
 #ifndef MOLPHENE_CONFIG_TYPE_HPP
 #define MOLPHENE_CONFIG_TYPE_HPP
 
+#include "detail/empty.hpp"
+
+namespace molphene {
+
 template<typename T>
 struct type_configs {
 private:
@@ -50,5 +54,16 @@ public:
   using int_type = typename int_type_imp<T, int>::type;
   using char_type = typename char_type_imp<T, char>::type;
 };
+
+template<>
+struct type_configs<void> : type_configs<detail::empty>
+{
+  using type_configs<detail::empty>::size_type;
+  using type_configs<detail::empty>::float_type;
+  using type_configs<detail::empty>::int_type;
+  using type_configs<detail::empty>::char_type;
+};
+
+}
 
 #endif
