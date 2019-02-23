5e5e58a267e05eb2c198957df843b66b3fe22beb
Januar Andaria <janucaria@gmail.com>
2019-02-23 10:52:16 +0800

    refactor: add int_type char_type in type_configs

--- libs/core/src/molphene/type_configs.hpp
+++ libs/core/src/molphene/type_configs.hpp
@@ -24,9 +24,31 @@ private:
     using type = typename U::float_type;
   };
 
+  template<typename U, typename V, typename = void>
+  struct int_type_imp {
+    using type = V;
+  };
+
+  template<typename U, typename V>
+  struct int_type_imp<U, V, std::void_t<typename U::int_type>> {
+    using type = typename U::float_type;
+  };
+
+  template<typename U, typename V, typename = void>
+  struct char_type_imp {
+    using type = V;
+  };
+
+  template<typename U, typename V>
+  struct char_type_imp<U, V, std::void_t<typename U::char_type>> {
+    using type = typename U::char_type;
+  };
+
 public:
   using size_type = typename size_type_imp<T, std::size_t>::type;
-  using float_type = typename float_type_imp<T, float>::type;
+  using float_type = typename float_type_imp<T, double>::type;
+  using int_type = typename int_type_imp<T, int>::type;
+  using char_type = typename char_type_imp<T, char>::type;
 };
 
 #endif
